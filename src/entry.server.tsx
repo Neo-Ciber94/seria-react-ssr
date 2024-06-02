ba5b9e0559d0499b9c9c7f7a5a76b1cc454f38b5
Freddy L <neo-ciber_94@hotmail.com>
2024-06-02 18:17:22 -0400

    refactor

--- src/entry.server.tsx
+++ src/entry.server.tsx
@@ -11,17 +11,16 @@ import path from "path";
 import { fileURLToPath } from "url";
 import * as seria from "seria";
 
-const isDev = process.env.NODE_ENV !== "production";
-const CLIENT_DIR = isDev ? "/build/client" : "/client";
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const baseDir = path.resolve(__dirname, "..");
-const rootDir = path.relative(process.cwd(), baseDir) || "./";
 
-const port = Number(process.env.PORT ?? 5000);
-const hostname = process.env.HOST ?? "localhost";
+const IS_DEV = process.env.NODE_ENV !== "production";
+const CLIENT_DIR = IS_DEV ? "/build/client" : "/client";
+const BASE_DIR = path.resolve(__dirname, "..");
+const ROOT_DIR = path.relative(process.cwd(), BASE_DIR) || "./";
+
 const app = new Hono();
 
-app.use(`${CLIENT_DIR}/*`, serveStatic({ root: rootDir }));
+app.use(`${CLIENT_DIR}/*`, serveStatic({ root: ROOT_DIR }));
 app.use(
   "/*",
   serveStatic({
@@ -42,7 +41,6 @@ app.get("*", async (ctx) => {
   }
 
   const { params = {}, ...route } = match;
-
   let loaderData: any = undefined;
 
   if (route.loader) {
@@ -140,13 +138,9 @@ function createResponse(appContext: AppContext) {
   });
 }
 
-serve(
-  {
-    fetch: app.fetch,
-    hostname,
-    port,
-  },
-  () => {
-    console.log(`Listening on http://${hostname}:${port}`);
-  }
-);
+const port = Number(process.env.PORT ?? 5000);
+const hostname = process.env.HOST ?? "localhost";
+
+serve({ fetch: app.fetch, hostname, port }, () => {
+  console.log(`Listening on http://${hostname}:${port}`);
+});
