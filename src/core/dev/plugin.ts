f46950597f136532989130c0556e9612950b0490
Freddy L <neo-ciber_94@hotmail.com>
2024-06-11 18:39:04 -0400

    remove esbuild

---
+++ src/core/dev/plugin.ts
@@ -0,0 +1,129 @@
+import { ConfigEnv, PluginOption } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import fs from "fs/promises";
+import { startViteServer } from "../server/vite";
+import { createClientServerActionProxyFromPath } from "./createClientServerActionProxy";
+import { removeServerExportsFromSource } from "./removeServerExports";
+import { getLoader } from "./utils";
+
+const routesDir = normalizePath(path.join(process.cwd(), "src/routes"));
+
+function isInRoutes(filePath: string) {
+  return filePath.startsWith(routesDir);
+}
+
+export function frameworkPlugin(config: ConfigEnv): PluginOption {
+  console.log(config);
+
+  if (config.isSsrBuild) {
+    return [];
+  }
+
+  return [
+    react(),
+    {
+      name: "dev-server",
+      async config() {
+        return {
+          appType: "custom",
+          optimizeDeps: {
+            entries: ["react", "react/jsx-runtime", "react/jsx-dev-runtime", "react-dom/client"],
+          },
+          build: {
+            minify: false,
+            rollupOptions: {
+              input: ["./src/entry.client.tsx"],
+              output: {
+                format: "es",
+                manualChunks(id) {
+                  const relativePath = normalizePath(path.relative(process.cwd(), id));
+                  const isReact =
+                    relativePath.startsWith("node_modules/react") ||
+                    relativePath.startsWith("node_modules/react-dom");
+
+                  if (isReact) {
+                    return "react";
+                  }
+
+                  if (isExternal(id)) {
+                    return "vendor";
+                  }
+
+                  if (!config.isSsrBuild) {
+                    if (isInRoutes(id)) {
+                      const chunkName = path
+                        .relative(routesDir, id)
+                        .replaceAll(path.sep, "/")
+                        .replaceAll("/", "_")
+                        .replaceAll(/\.(j|t)sx?$/g, "");
+
+                      return `routes_${chunkName}`;
+                    }
+                  }
+                },
+              },
+            },
+          },
+        };
+      },
+      async configureServer(server) {
+        return async () => {
+          if (!server.config.server.middlewareMode) {
+            await startViteServer(server);
+          }
+        };
+      },
+    },
+    {
+      name: "create-server-action-proxy",
+      enforce: "pre",
+      async load(id) {
+        if (isExternal(id) || !/_actions\.(js|ts|jsx|tsx)$/.test(id) || !isInRoutes(id)) {
+          return;
+        }
+
+        const result = await createClientServerActionProxyFromPath(id);
+
+        return {
+          code: result.code,
+        };
+      },
+    },
+    {
+      name: "remove-server-exports",
+      enforce: "pre",
+      async load(id) {
+        if (isExternal(id) || !/\.(js|ts|jsx|tsx)$/.test(id) || !isInRoutes(id)) {
+          return;
+        }
+
+        const contents = await fs.readFile(id, "utf8");
+        const loader = getLoader(id);
+        const result = await removeServerExportsFromSource(contents, loader);
+
+        return {
+          code: result.code,
+        };
+      },
+    },
+    {
+      name: "ignore-server-files",
+      resolveId(source) {
+        if (/\.server\.(ts|js|tsx|jsx)$/.test(source)) {
+          return { id: source, external: true };
+        }
+
+        return null;
+      },
+    },
+  ];
+}
+
+function normalizePath(filepath: string) {
+  return filepath.replaceAll(path.win32.sep, path.posix.sep);
+}
+
+function isExternal(id: string) {
+  return id.includes("/node_modules/");
+}
